1669d1dd645a3db0b5b6f117ccc807956f6756a3
Pemapol Sripratipbundit <pemapolsripratipbundit@gmail.com>
2024-10-22 18:28:39 +0700

    front victory

--- client/src/components/victory.jsx
+++ client/src/components/victory.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import axios from "axios";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { disconnectSocket } from '../socket';
-
+import avatarWin from "../assets/images/avatar-win.png";
+import victoryHeader from "../assets/images/victory-header.png";
 
 const Victory = () => {
   // Declare a state variable 'count' and a setter function 'setCount'
@@ -30,28 +31,59 @@ const Victory = () => {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Counter: {count}</h2>
-      
-      {/* Button to increment the count */}
-      <button onClick={handleRematch} style={buttonStyle}>
-        Rematch
-      </button>
+    // <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <section class="flex h-screen w-screen bg-gray-500 justify-center items-center">
+    <div class="flex flex-col justify-center h-700 w-300">
+      <div class="flex justify-center items-center">
+      <img 
+          src={victoryHeader}
+          width= {800}
+          class=""
+      />
+      </div>
+      <div class="flex justify-center items-center">
+        <h2 class="font-montserrat text-2xl text-white font-bold pb-10">
+          Great work, NAME. {/**put name var here */}
+        </h2>
+      </div>
+      <div class="flex flex-col justify-between items-center gap-2 pb-20">
+        <div class="flex flex-1 items-center w-3/5">
+          <p class="font-montserrat font-medium text-3xl text-white w-96">NAME1</p>
+          <div class="flex gap-8 w-48 justify-center">
+            <p class="font-montserrat font-medium text-3xl w-12 text-white text-center">1</p>
+            <div class="bg-green-700 w-24">
+              <p class="m-2 font-montserrat text-center">STATUS</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-1 items-center w-3/5">
+          <p class="font-montserrat font-medium text-3xl text-white w-96">NAME2</p>
+          <div class="flex gap-8 w-48 justify-center">
+            <p class="font-montserrat font-medium text-3xl w-12 text-white text-center">0</p>
+            <div class="bg-cyan-500 w-24">
+              <p class="m-2 font-montserrat text-center">STATUS</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      {/* Button to decrement the count */}
-      <button onClick={handleDisconnect} style={buttonStyle}>
-        Disconnect
-      </button>
+      <div class="grid grid-cols-3 grid-rows-2 gap-40">
+        <button onClick={handleRematch} className="row-start-2 flex justify-center items-center gap-2 px-7 py-4 border-4 font-montserrat font-bold text-xl leading-none ring-4 ring-white text-white rounded-full bg-sky-700">
+            Rematch
+        </button>
+        <img
+          src={avatarWin}
+          height={100}
+          width={200}
+          class="row-start-1 row-span-2"
+        />
+        <button onClick={handleDisconnect} className="row-start-2 flex justify-center items-center gap-2 px-7 py-4 border-4 font-montserrat font-bold text-xl leading-none ring-4 ring-white text-white rounded-full bg-sky-700">
+            Disconnect
+        </button>
+      </div>
     </div>
+    </section>
   );
-};
-
-// Optional inline styling for buttons
-const buttonStyle = {
-  padding: '10px 20px',
-  margin: '10px',
-  fontSize: '16px',
-  cursor: 'pointer',
 };
 
 export default Victory;
