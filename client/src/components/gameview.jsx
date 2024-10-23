70820ebef7196e61bc6aad8ff3ed60fd0f9e3b40
MPSC <maipro13876@gmail.com>
2024-10-24 00:09:49 +0700

    improve gameview

--- client/src/components/gameview.jsx
+++ client/src/components/gameview.jsx
@@ -5,26 +5,38 @@ const Gameview = () => {
   const [grid, setGrid] = useState([]);
 
   const initGrid = () => {
-    const grid = Array(2).fill(null)
+    const igrid = Array(2).fill(null)
     const playerGrid = <PlaceshipGrid />;
-    grid[0] = playerGrid;
-    grid[1] = playerGrid;
-    setGrid(grid)
+    const button = <button onClick={() => addNewItemToGrid()}>Add Item</button>
+    console.log("yooooooooooo")
+    igrid[0] = playerGrid;
+    igrid[1] = button;
+    setGrid(igrid)
   }
  
   useEffect(() => {
     initGrid();
   }, []);
 
-   // Function to add a new item to the grid (not care about dragdrop)
-  const addNewItemToGrid = (index) => {
+   // Function to start playing game after placeship
+  const addNewItemToGrid = () => {
     const updatedGrid = [...grid];
-    updatedGrid[index] = 'drag-item'; // Adding a simple string to represent the item
+
+    const playerGrid = <PlaceshipGrid />;
+
+    //update playergrid(playerGrid)
+    updatedGrid[0] = playerGrid; 
+
+    //add enemygrid
+
+    //append all grid
+
+    updatedGrid[1] = playerGrid; 
     setGrid(updatedGrid);
   };
 
   return (
-    <div   style={{display: 'flex'}}>
+    <div style={{display: 'flex'}}>
       {grid.map((item, index) => (
         <div key={index}>
           {item ? item : 'Empty Grid'}
