295b75ad4ce1a63b5e58e560602d4a5e33ba1b4b
HoonPK <hoon.leelawa@gmail.com>
2024-11-07 03:41:07 +0700

    vid fit screen, autoPlay+muted+loop, center using px for now, code in landing for now

--- client/src/components/lore.jsx
+++
@@ -1,7 +0,0 @@
-import "./css/style.css";
-
-<div className='overflow-x-hidden'>
-                  {/* background */}
-                  <video src={process.env.PUBLIC_URL + "/assets/vids/lore-vid.mp4"} loop autoPlay muted className='object-cover absolute h-screen w-screen -z-10 top-0 left-0'></video>
-                </div>
-
