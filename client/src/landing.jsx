a86d7e5497bd99bacc2c2dc0751328cbdcaf1f03
HoonPK <hoon.leelawa@gmail.com>
2024-11-07 03:21:34 +0700

    all white

--- client/src/landing.jsx
+++ client/src/landing.jsx
@@ -15,6 +15,7 @@ const App = () => {
                     width={750}
                     className="pt-24"
                 />
+
                 <div className="flex gap-10 pt-20">
                   <a href={"/loginSimple"}>
                   <button className="flex justify-center items-center gap-2 px-7 py-4 border-4 font-montserrat font-bold text-xl leading-none border-white text-white rounded-full bg-sky-700 dark:bg-sky-950 hover:bg-green-800">
