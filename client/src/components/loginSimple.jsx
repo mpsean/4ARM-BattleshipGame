efee9e97db78378701ec21c6c278b5ddccc7d3f1
HoonPK <hoon.leelawa@gmail.com>
2024-11-07 22:27:11 +0700

    Better profile search, loading data broken

--- client/src/components/loginSimple.jsx
+++ client/src/components/loginSimple.jsx
@@ -54,7 +54,7 @@ function loginSimple({ setIsLoggedIn, isLoggedIn }) {
 
       
       {/* Form submission logic with React */}
-      <form class="font-montserrat justify-center text-center bg-white dark:bg-cyan-800 m-2 p-5 border-1 rounded-2xl shadow-lg" id="nameForm" onSubmit={handleSubmit}>
+      <form class="font-montserrat justify-center text-center bg-sky-300 dark:bg-cyan-800 m-2 p-5 border-1 rounded-2xl shadow-lg" id="nameForm" onSubmit={handleSubmit}>
         {/* Label omitted for simplicity */}
         <input 
           type="text" 
