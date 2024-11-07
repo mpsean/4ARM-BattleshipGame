7f183d6fd35371258b62dee654d5c672950e247f
Pemapol Sripratipbundit <pemapolsripratipbundit@gmail.com>
2024-11-07 22:53:36 +0700

    user profile edits

--- client/src/components/userProfile.jsx
+++ client/src/components/userProfile.jsx
@@ -32,29 +32,35 @@ const Profile = () => {
   };
 
   return (
-    <div className="min-h-screen min-w-screen bg-sky-300 dark:bg-sky-900">
+    <div className="flex min-h-screen min-w-screen justify-center items-center bg-sky-300 dark:bg-sky-900">
+      <div className="flex flex-col justify-center w-5/6 max-w-screen-md">
       <h2  className="text-center font-montserrat pt-4 font-bold text-3xl text-sky-900 m-2 dark:text-white">User Profile</h2>
       {/* Conditional rendering based on the userData state */}
       {userData === null ? (
-        <p className="text-center font-montserrat pt-4 font-normal text-3xl text-sky-900 m-2 dark:text-white">Loading data...</p> // Show a loading message if data hasn't been fetched yet
+        <p className="text-center font-montserrat p-8 font-normal text-3xl text-sky-900 m-2 dark:text-white">User Not Found.</p> // Show a loading message if data hasn't been fetched yet
       ) : (
         <div>
-          <h3 className="text-center font-montserrat pt-4 font-bold text-6xl text-sky-900 m-2 dark:text-white">{userData.nickname}</h3>
+          <h3 className="text-center font-museo pt-4 font-bold text-6xl text-sky-900 m-2 dark:text-white">{userData.nickname}</h3>
           <div className="m-10 rounded-lg bg-white dark:bg-sky-700 py-1">
-            <p className="text-center font-montserrat pt-4 font-normal text-xl text-sky-900 m-2 dark:text-white">Matches Played: {userData.matchPlayed}</p>
-            <p className="text-center font-montserrat pt-4 font-normal text-xl text-sky-900 m-2 dark:text-white">Matches Won: {userData.matchWon}</p>
-            <p className="text-center font-montserrat pt-4 font-normal text-xl text-sky-900 m-2 dark:text-white">Matches Lost: {userData.matchLose}</p>
-            <p className="text-center font-montserrat pt-4 font-normal text-xl text-sky-900 m-2 dark:text-white">Matches Draw: {userData.matchDraw}</p>
+            <p className="text-center font-montserrat p-4 font-medium text-xl text-sky-900 m-2 dark:text-white">Matches Played: {userData.matchPlayed}</p>
+            <p className="text-center font-montserrat p-4 font-medium text-xl text-sky-900 m-2 dark:text-white">Matches Won: {userData.matchWon}</p>
+            <p className="text-center font-montserrat p-4 font-medium text-xl text-sky-900 m-2 dark:text-white">Matches Lost: {userData.matchLose}</p>
+            <p className="text-center font-montserrat p-4 font-medium text-xl text-sky-900 m-2 dark:text-white">Matches Draw: {userData.matchDraw}</p>
           </div>
         </div>
       )}
+      <div className="flex justify-center">
+        <button
+          onClick={handleBack}
+          className="gap-2 px-7 py-4 border-4 h-16 max-w-32 font-montserrat font-bold text-xl leading-none text-white rounded-full bg-sky-700 dark:bg-sky-950 hover:bg-green-800"
+        >
+          Back
+        </button>
+      </div>
       
-      <button
-        onClick={handleBack}
-        className="justify-center items-center gap-2 px-7 py-4 border-4 h-16 font-montserrat font-bold text-xl leading-none text-white rounded-full bg-sky-700 dark:bg-sky-950 hover:bg-green-800"
-      >
-        Back
-      </button>
+
+      </div>
+      
     </div>
   );
 };
